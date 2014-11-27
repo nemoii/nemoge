56a52c67d47bff2b7b2e6f6c1ae94c01107a210a
Nemo <me@nemoge.com>
2014-11-27 19:57:59 +0800

    exercise

--- exercise/js/account.js
+++ exercise/js/account.js
@@ -1,14 +1,134 @@
-﻿
+﻿var xmlHttp;
+
 function init()
 {
-	if (window.location.search == "")
+	xmlHttp = GetXmlHttpObject();
+	if (xmlHttp == null)
 	{
+		alert ("Browser does not support HTTP Request");
+		return;
+	} 
+
+	if (window.location.search != "")
+	{
+		initList();
 		return;
 	}
+	
+	xmlHttp.onreadystatechange = onGetStat;
+	xmlHttp.open("GET", "api.php?event=stat", true);
+	xmlHttp.send(null);
+}
 
-	title = document.getElementsByTagName("title")[0];
+function onGetStat() 
+{ 
+	if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete")
+	{ 
+		var stat = xmlHttp.responseXML.getElementsByTagName("Stat")[0].firstChild.data;
+		var btn = window.document.getElementById("btn");
+		switch(stat)
+		{
+		case "0":
+			btn.textContent = "通信错误";
+			break;
+		case "1":
+			btn.textContent = "开始！";
+			btn.disabled = false;
+			btn.onclick = function(){start()};
+			break;
+		case "2":
+			btn.textContent = "停止！";
+			btn.disabled = false;
+			btn.onclick = function(){stop()};
+			break;
+		case "3":
+			btn.textContent = "已结束";
+			break;
+		}
+	} 
+}
+
+function start()
+{
+	btn.disabled = true;
+	xmlHttp.onreadystatechange = onStarted;
+	xmlHttp.open("GET", "api.php?event=start", true);
+	xmlHttp.send(null);
+}
+
+function stop()
+{
+	btn.disabled = true;
+	xmlHttp.onreadystatechange = onStoped;
+	xmlHttp.open("GET", "api.php?event=stop", true);
+	xmlHttp.send(null);
+}
+
+function onStarted() 
+{ 
+	if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete")
+	{ 
+		var flag = xmlHttp.responseXML.getElementsByTagName("Flag")[0].firstChild.data;
+		var btn = window.document.getElementById("btn");
+		
+		if(flag == "1")
+		{
+			btn.textContent = "已开始";
+		}
+		else
+		{
+			btn.textContent = "错误";
+		}
+	}
+}
+
+function onStoped() 
+{ 
+	if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete")
+	{ 
+		var flag = xmlHttp.responseXML.getElementsByTagName("Flag")[0].firstChild.data;
+		var btn = window.document.getElementById("btn");
+		
+		if(flag == "1")
+		{
+			btn.textContent = "已结束";
+		}
+		else
+		{
+			btn.textContent = "错误";
+		}
+	}
+}
+
+function initList()
+{
 	wrapper = document.getElementById("wrapper");
 }
+
+function GetXmlHttpObject()
+{
+	var xmlHttp = null;
+
+	try
+	{
+		// Firefox, Opera 8.0+, Safari
+		xmlHttp = new XMLHttpRequest();
+	}
+	catch (e)
+	{
+		// Internet Explorer
+		try
+		{
+			xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+		}
+		catch (e)
+		{
+			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	}
+	return xmlHttp;
+}
+
 function getQueryStringArgs()
 {
     var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
